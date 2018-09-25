a52cb94dfcd1e4bc03e2c732fc878f56771352ba
kztai <1012049531@qq.com>
2018-09-25 22:32:50 +0800

    头部正式完成

--- elema/src/components/goods/goods.vue
+++ elema/src/components/goods/goods.vue
@@ -1,7 +1,49 @@
 <template>
-	<div>i am goods</div>
+	<div class="goods">
+		<div class="menu-wrap"></div>
+		<div class="foods-wrap"></div>
+	</div>
 </template>
 <script type="text/javascript">
-	export default {};
+	const ERR_OK = 0;
+
+	export default {
+		props: {
+			seller: {
+				type: Object
+			}
+		},
+		data() {
+			return {
+				goods: []
+			};
+		},
+		created() {
+			this.$http.get('/api/goods').then((data) => {
+				if (data.body.errno === ERR_OK) {
+					this.goods = data.body.data;
+					console.log(data.body.data);
+				}
+			});
+		}
+	};
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+	.goods {
+		display: flex;
+		position: absolute;
+		top: 177px;
+		bottom: 64px;
+		width: 100%;
+		height: 100%;
+		.menu-wrap {
+			flex: 0 0 80px;
+			/*为了处理兼容问题*/
+			width: 80px;
+			background-color: #f3f5f7;
+		}
+		.foods-wrap {
+			flex: 1;
+		}
+	}
+</style>
--- elema/src/components/header/header.vue
+++ elema/src/components/header/header.vue
@@ -30,30 +30,46 @@
 		<div class="background">
 			<img :src="seller.avatar" width="100%" height="100%">
 		</div>
-		<!-- 经典的sticky footer 布局 -->
-		<div v-show="detailShow" class="detail">
+		<transition name="fade">
+			<!-- 经典的sticky footer 布局 -->
+			<div v-show="detailShow" class="detail">
 			<!-- 这个层的作用是使遮罩能够铺满整个屏幕 -->
 			<div class="detail-wrap clearfix">
 				<div class="detail-main">
-					<p>{{seller.bulletin}}</p>
-					<p>{{seller.bulletin}}</p>
-					<p>{{seller.bulletin}}</p>
-					<p>{{seller.bulletin}}</p>
-					<p>{{seller.bulletin}}</p>
-					<p>{{seller.bulletin}}</p>
-					<p>{{seller.bulletin}}</p>
-					<p>{{seller.bulletin}}</p>
-					<p>{{seller.bulletin}}</p>
+					<h1 class="name">{{seller.name}}</h1>
+					<div class="star-wrap">
+						<star v-bind:size="48" v-bind:score="seller.score"></star>
+					</div>
+					<div class="title">
+						<div class="line"></div>
+						<div class="text">优惠信息</div>
+						<div class="line"></div>
+					</div>
+					<ul class="supports" v-if="seller.supports">
+						<li class="supports-item" v-for="(item,index) in seller.supports" v-bind:key="index">
+							<span class="icon" v-bind:class="classMap[seller.supports[index].type]"></span>
+							<span class="text">{{seller.supports[index].description}}</span>
+						</li>
+					</ul>
+					<div class="title">
+						<div class="line"></div>
+						<div class="text">商家公告</div>
+						<div class="line"></div>
+					</div>
+					<p class="bulletin-text">{{seller.bulletin}}</p>
 				</div>
 			</div>
 			<div class="detail-close">
-				<i class="icon-close"></i>
+				<i class="icon-close" v-on:click="closeDetail"></i>
 			</div>
-		</div>
+			</div>
+		</transition>
 	</div>
 </template>
 
 <script type="text/javascript">
+	import star from '../star/star.vue';
+
 	export default {
 		// 接收父级传来的参数
 		props: {
@@ -69,10 +85,16 @@
 		methods: {
 			showDetail() {
 				this.detailShow = true;
+			},
+			closeDetail() {
+				this.detailShow = false;
 			}
 		},
 		created() {
-			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guraantee'];
+			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+		},
+		components: {
+			star
 		}
 	};
 </script>
@@ -203,13 +225,92 @@
 			z-index: 100;
 			/*为了让遮罩层能够滑动*/
 			overflow: auto;
+			backdrop-filter: blur(10px);
+			opacity: 1;
 			background-color: rgba(7,17,27,0.8);
+			&.fade-enter-active,&.fade-leave-active {
+				transition: all 0.5s;
+			}
+			&.fade-enter,&.fade-leave-active {
+				opacity: 0;
+				background-color: rgba(7,17,27,0);
+			}
 			.detail-wrap {
 				min-height: 100%;
 				.detail-main {
 					margin-top: 64px;
+					padding: 0 36px;
 					/*为了让关闭按钮不被内容覆盖*/
 					padding-bottom: 64px;
+
+					text-align: center;
+					.name {
+						font-size: 16px;
+						line-height: 16px;
+						font-weight: 700;
+					}
+					.star-wrap {
+						padding-top: 16px;
+					}
+					.title {
+						display: flex;
+						padding: 28px 0 24px 0;
+						.line {
+							flex: 1;
+							border-bottom: 1px solid rgba(255,255,255,0.2);
+							margin: auto 0;
+						}
+						.text {
+							flex: 1;
+							padding: 0 12px;
+							font-size: 14px;
+							font-weight: 700;
+						}
+					}
+					.supports {
+						text-align: left;
+						padding-left: 12px;
+						.supports-item {
+							padding-bottom: 12px;
+							&:last-child {
+								padding-bottom: 0;
+							}
+							.text {
+								font-size: 12px;
+								line-height: 16px;
+							}
+							.icon {
+								display: inline-block;
+								width: 16px;
+								height: 16px;
+								background-repeat: no-repeat;
+								background-size: 16px 16px;
+								margin-right: 6px;
+								vertical-align: top;
+								&.decrease {
+									@include bg-image('decrease_2');
+								}
+								&.discount {
+									@include bg-image('discount_2');
+								}
+								&.guarantee {
+									@include bg-image('guarantee_2');
+								}
+								&.invoice {
+									@include bg-image('invoice_2');
+								}
+								&.special {
+									@include bg-image('special_2');
+								}
+							}
+						}
+					}
+					.bulletin-text {
+						line-height: 24px;
+						font-size: 12px;
+						text-align: left;
+						padding: 0 12px;
+					}
 				}
 			}
 			.detail-close {
