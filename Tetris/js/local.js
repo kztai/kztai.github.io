a21b95ccbd8f8d5139a4a235591489ce7a5aff0e
kzt <test@e.com>
2021-03-28 22:44:28 +0800

    'tetris'

---
+++ Tetris/js/local.js
@@ -0,0 +1,96 @@
+var Local = function() {
+	// 游戏对象
+	var game;
+	// 定时器
+	var timer = null;
+	// 时间间隔
+	var INTERVAL = 200;
+	// 时间计数器
+	var timeCount = 0;
+	// 时间
+	var time = 0;
+
+	// 绑定键盘事件
+	var bindKeyEvent = function() {
+		document.onkeydown = function(e) {
+			if (e.keyCode === 38) {  //up
+				game.rotate();
+			} else if (e.keyCode === 39) {  //right
+				game.right();
+			} else if (e.keyCode === 40) {  //down
+				game.down();
+			} else if (e.keyCode === 37) {  //left
+				game.left();
+			} else if (e.keyCode === 32) {  //space
+				game.fall();
+			}
+		};
+	};
+
+	// 移动
+	var move = function() {
+		timeFunc();
+		if(!game.down()) {
+			game.fixed();
+			var line = game.checkClear();
+			if (line) {
+				game.addScore(line);
+			}
+			if (game.checkGameOver()) {
+				game.gameover(false);
+				stop();
+			} else {
+				game.performNext(generateType(), generateDir());
+			}
+		}
+	};
+
+	// 计时函数
+	var timeFunc = function() {
+		timeCount++;
+		if (timeCount === 5) {
+			timeCount = 0;
+			time++;
+			game.setTime(time);
+		}
+	};
+
+	// 随机生成一个方块种类
+	var generateType = function() {
+		return Math.ceil(Math.random() * 7) - 1;
+	};
+
+	// 随机生成一个旋转次数
+	var generateDir = function() {
+		return Math.ceil(Math.random() * 4) - 1;
+	};
+
+	// 开始
+	var start = function() {
+		var doms = {
+			gameDiv: document.getElementById('game'),
+			nextDiv: document.getElementById('next'),
+			timeDiv: document.getElementById('time'),
+			scoreDiv: document.getElementById('score'),
+			resultDiv: document.getElementById('gameover')
+		};
+
+		game = new Game();
+		game.init(doms, generateType(), generateDir());
+		bindKeyEvent();
+		game.performNext(generateType(), generateDir());
+		timer = setInterval(move, INTERVAL);
+	};
+
+	// 结束
+	var stop = function() {
+		if (timer) {
+			clearInterval(timer);
+			timer = null;
+		}
+		document.onkeydown = null;
+	};
+
+	// 导出API
+	this.start = start;
+};
